61b084d4378d544b19e6943b428557e16c67eff0
prakash <boharap7788@gmail.com>
2023-11-17 21:40:35 +0530

    code is added

--- src/main/java/com/springApp/SpringBootProjectApplication.java
+++ src/main/java/com/springApp/SpringBootProjectApplication.java
@@ -2,9 +2,18 @@ package com.springApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootProjectApplication {
+	
+	
+	@GetMapping("/cicdapp")
+	public String cicdApp() {
+		return "CI-CD Automation Set";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootProjectApplication.class, args);
